49393f58b1d65d711fa4990208d3463f77acf5b8
ladcam <ladcam90@gmail.com>
2023-11-28 18:20:45 -0500

    fourth commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { Pool } = require('pg');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.use(express.json());
 require('dotenv').config()  
 
